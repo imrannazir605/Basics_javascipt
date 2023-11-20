308d11ab1db0fa56c7c90ad8c7ff37a8cdb72bc8
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-20 02:39:45 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+let accounId = 32145;
+const accountName = "imran";
+var accountTitle = "nazir"
+accountCountries = "pakistan";
+
+
+//console.log(accounId);
+console.table([accounId,accountName,accountTitle,accountCountries]);
