8bdcb4d30d9b78749349a8035af09bab4f2b4853
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-20 10:48:00 +0000

    container config done

---
+++ 01_basics/02_conversionoperation.js
@@ -0,0 +1,9 @@
+let score = "33abc" ;
+let userName = "ahmad"
+ 
+let theScoreof = Number(score)
+console.log(typeof theScoreof);
+
+
+let nuserName = Number(userName)
+//console.log(typeof nuserName);
