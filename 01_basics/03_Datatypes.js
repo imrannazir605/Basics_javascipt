8ac06c837173182d3693068ed376e30398faf36f
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-22 05:53:12 +0000

    container config done

---
+++ 01_basics/03_Datatypes.js
@@ -0,0 +1,22 @@
+let name = "ahmad"
+console.log(typeof name);
+
+let score = 34;
+console.log(typeof score);
+
+
+let ifuserLogin = false;
+console.log(typeof ifuserLogin);
+
+
+let temp = null;
+console.log(typeof temp);
+
+
+
+let subscore = undefined;
+console.log(typeof subscore);
+
+
+let bigNumber = 1234564738273;
+console.log(typeof bigNumber);
