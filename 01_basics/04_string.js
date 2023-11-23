67c0f795dd619263ff2a8d294d280d234f7ce5b4
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-23 17:03:22 +0000

    container config done

--- 01_basics/04_string.js
+++ 01_basics/04_string.js
@@ -1,3 +1,8 @@
 let gameName = new String ("pubgmobilelite");
 
-console.log(gameName);
+// console.log(gameName.__proto__);
+console.log(gameName.charAt());
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+// console.log(gameName.fontcolor());
+console.log(gameName.search());
