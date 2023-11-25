9e5411c9f23ae301fa5eedcb395a54b03b9f064f
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-25 05:42:33 +0000

    container config done

---
+++ 01_basics/06_Date_timeIn.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
