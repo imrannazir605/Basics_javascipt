2424d2dabab917821c99c714fd5c9d98429898f2
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-27 07:35:47 +0000

    container config done

---
+++ 02_basics/02_object.js
@@ -0,0 +1,22 @@
+//object literals method
+const myobj = {
+    name : "imran",
+    "fullname" : "imran nazir",
+    age : 23,
+    email : "imran@gmail.com",
+    userlogin : false,
+}
+
+console.log(myobj.age);
+console.log(myobj["fullname"]);
+console.log(myobj["email"]);
+console.log(myobj.userlogin);
+
+const myobj2 = {}
+ 
+myobj2.id = "123"
+myobj2.name = "imran"
+myobj2.email = "imran@gmail.com"
+
+
+console.log(myobj2.id);
