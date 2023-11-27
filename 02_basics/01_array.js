0f5ab8c00b78e1111402e8659747f0faa438ef97
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-27 09:21:07 +0000

    container config done

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -18,9 +18,19 @@ console.log(myArr.includes(8));
 console.log(myArr.includes(5));
 
 
-const newArray = myArr.join();
-console.log(typeof newArray);
+// const newArray = myArr.join();
+// console.log(typeof newArray);
 
-console.log('A',myArr);
-const myn1 = myArr.slice(1,3);
-console.log(myn1);
+// console.log('A',myArr);
+// const myn1 = myArr.slice(1,3);
+// console.log(myn1);
+
+
+const heros1 = ["shaktiman","ironman","badman"]
+const heros2 = ["salman","usman bay","turgut alp"]
+
+// const all_heros = heros1.concat(heros2)
+// const all_heros = heros1.join(heros2)
+console.log(all_heros);
+
+
--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -1,16 +1,16 @@
 //object literals method
-const myobj = {
-    name : "imran",
-    "fullname" : "imran nazir",
-    age : 23,
-    email : "imran@gmail.com",
-    userlogin : false,
-}
+// const myobj = {
+//     name : "imran",
+//     "fullname" : "imran nazir",
+//     age : 23,
+//     email : "imran@gmail.com",
+//     userlogin : false,
+// }
 
-console.log(myobj.age);
-console.log(myobj["fullname"]);
-console.log(myobj["email"]);
-console.log(myobj.userlogin);
+// console.log(myobj.age);
+// console.log(myobj["fullname"]);
+// console.log(myobj["email"]);
+// console.log(myobj.userlogin);
 
 const myobj2 = {}
  
@@ -19,4 +19,34 @@ myobj2.name = "imran"
 myobj2.email = "imran@gmail.com"
 
 
-console.log(myobj2.id);
+// console.log(myobj2.id);
+
+
+// const obj3 = {1:"a",2:"B"}
+// const obj4 = {3 : "c", 4: "D"}
+// // const obj5 = Object.assign({}, obj3, obj4)
+// const obj5 = {...obj3,...obj4}
+// console.log(obj5);
+
+// const userlogindata = [
+//     {
+//     id : 123,
+//     email: "imran@gmail.com"
+//     },
+//     {
+//         id : 123,
+//         email: "imran@gmail.com"
+//      },
+
+//     {
+//             id : 123,
+//             email: "imran@gmail.com"
+//      },
+// ]
+
+// userlogindata[1].email
+console.log(myobj2);
+
+console.log(Object.keys(myobj2));
+console.log(Object.values(myobj2));
+console.log(Object.entries(myobj2));
