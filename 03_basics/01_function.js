14d193f9a85ea13cbc3982f884d6de6da27fbe8b
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-29 08:45:38 +0000

    container config done

---
+++ 03_basics/01_function.js
@@ -0,0 +1,13 @@
+// function AddTwonumbers(num1,num2) {
+//     return num1 + num2;
+    
+// }
+// let result = AddTwonumbers(3,3)
+// console.log("result:", result);
+
+
+function userlogin(username) {
+    return `${username} justloged in`
+    
+}
+console.log(userlogin("imran"));
