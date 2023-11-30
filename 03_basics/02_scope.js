21191d775c44767ae7054b6c8226cca538fdba09
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-11-30 10:03:44 +0000

    container config done

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,13 @@
+function user() {
+   const name = "imran"
+    function age(){
+        const age = 23
+        console.log(name);
+    }
+    //console.log(age)
+
+    age()
+    
+    
+}
+user()
