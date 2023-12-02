bc499672d113aedf58e25e32803b2cb51dc52f6d
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-12-02 13:28:35 +0000

    container config done

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,21 @@
+// const user = {
+//     username : "imran",
+
+//     wellcomeMessage: function () {
+//         console.log(`${this.username} welcome to the website`);
+        
+//     }
+//     // console.log(this.username);
+// }
+// user.wellcomeMessage()
+// user.username = "saqib"
+// user.wellcomeMessage()
+const user = {
+    username : "hamad",
+    age : 23,
+    userMessage : function(){
+        console.log(`${this.username} , wellcome to our website`);
+    }
+
+}
+user.userMessage()
