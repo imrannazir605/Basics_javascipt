fcb1977c7b1b0ad9f8e8a99fe1173034fdb657b9
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-12-05 17:49:13 +0000

    container config done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+// const isuserlogedin = true 
+// if (isuserlogedin) {
+//     console.log("user is loged in");   
+// }
+
+
+//  const temprature = 41
+//  if (temprature > 41) {
+//     console.log("the temprature is greater then 41");
+//  }
+//  else{
+//     console.log("no the temp is less then 41");
+//  }
+
+
+const score = 70
+if (score >= 80 && score <= 100) {
+    console.log("congratulation you got A grade"); 
+}
+else if (score >= 70 && score < 80){
+    console.log("you take B grade");
+}
+else{
+    "sorry you are failed try again"
+}
