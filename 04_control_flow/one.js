bc26a9f0439411628f21e66baa7ec7928a7d78b9
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-12-05 17:55:58 +0000

    config container done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -13,13 +13,25 @@
 //  }
 
 
-const score = 70
-if (score >= 80 && score <= 100) {
-    console.log("congratulation you got A grade"); 
-}
-else if (score >= 70 && score < 80){
-    console.log("you take B grade");
-}
-else{
-    "sorry you are failed try again"
-}
+// const score = 70
+// if (score >= 80 && score <= 100) {
+//     console.log("congratulation you got A grade"); 
+// }
+// else if (score >= 70 && score < 80){
+//     console.log("you take B grade");
+// }
+// else{
+//     "sorry you are failed try again"
+// }
+
+
+// const userIslogedin = true
+// const Debidcard = true
+// const googleaccountlogedin = false
+// const emailaccountlogedin = true
+// if (userIslogedin && Debidcard) {
+//     console.log("congratulation you are logedin now you can do the shoping");
+// }
+// if (googleaccountlogedin || emailaccountlogedin) {
+//     console.log("now you can teake shopping");
+// }
