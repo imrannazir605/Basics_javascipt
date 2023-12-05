ddaa90f2e7cff7ae17d37891464d167802fa3250
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-12-05 18:16:02 +0000

    container config done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month = "april"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("febuary");
+        break;
+    case "martch":
+        console.log("martch");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default vale")
+        break;
+}
