d7312074ffbae366db87d1fbee0698c9287d2293
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-12-06 18:38:16 +0000

    container config done

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+// let i = 1
+// while (i <= 10) {
+//     console.log(`the value of i is ${i}`);
+//     i++
+// }
+
+
+// let myarray = ['turgut','bamsi','usman']
+// let index = 0
+// while (index < myarray.length) {
+//     console.log(`the super heros is ${myarray[index]}`);
+//     index = index + 1
+    
+// } 
+
+// let index = 0
+// do {
+//     console.log(`the value is ${index}`);
+//     index = index + 2
+// } while (index <= 20)
