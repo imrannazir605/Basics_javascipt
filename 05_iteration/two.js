545ec6052803cd2a9adba8ef8b95beafa0956518
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-12-07 19:54:05 +0000

    container config done

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -18,3 +18,21 @@
 //     console.log(`the value is ${index}`);
 //     index = index + 2
 // } while (index <= 20)
+
+const greeting = "Hello world"
+for (const greet of greeting) {
+    // console.log(greet);
+}
+
+let array = [10,12,14,16,18]
+for (const newarray of array) {
+    // console.log(newarray);   
+}
+
+
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('AUS', 'ASUTRALIA')
+map.set('pak', 'Pakistan')
+
+console.log(`the values of the coutries ${map}`)
