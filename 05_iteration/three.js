e5be9e2e06a76856eff75ebbaf35caa758a5b7fd
imrannazir605 <113233182+imrannazir605@users.noreply.github.com>
2023-12-08 10:07:17 +0000

    container config done

---
+++ 05_iteration/three.js
@@ -0,0 +1,15 @@
+// const myobj = {
+//     js : "java script",
+//     cpp : "c++",
+//     ja : "java"
+// }
+
+// for (const key in myobj) {
+//     console.log(`the  ${key} stand for ${myobj[key]}`);
+// }
+
+// const arr = ["javascript","c++","Android","Html"]
+// for (const key in arr) {
+//     console.log(`${arr[key]}` );
+
+// }
