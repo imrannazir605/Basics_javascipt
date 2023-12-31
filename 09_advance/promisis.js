8cf15dbdf83e7ee15c6746355db0285e03da70b0
imran nazir <113233182+imrannazir605@users.noreply.github.com>
2023-12-31 05:54:01 +0000

    container config done

--- 09_advance/promisis.js
+++ 09_advance/promisis.js
@@ -1,80 +1,91 @@
 
-//promisis method one
-const promisesOne = new Promise( function (resolve,reject){
-    setTimeout( function(){
-        console.log("asycronus is complete");
-        resolve()
-    })
+// //promisis method one
+// const promisesOne = new Promise( function (resolve,reject){
+//     setTimeout( function(){
+//         console.log("asycronus is complete");
+//         resolve()
+//     })
    
-}) 
-promisesOne.then(function(){
-    console.log("2nd part runing");
-})
+// }) 
+// promisesOne.then(function(){
+//     console.log("2nd part runing");
+// })
 
-//promises method two
-new Promise( function (resolve,reject){
-    setTimeout(function (){
-        console.log("asyncrous runing");
-        resolve()
-    },1000)
-}).then(function(){
-    console.log("syncronus part two is runing");
-})
+// //promises method two
+// new Promise( function (resolve,reject){
+//     setTimeout(function (){
+//         console.log("asyncrous runing");
+//         resolve()
+//     },1000)
+// }).then(function(){
+//     console.log("syncronus part two is runing");
+// })
 
-//promisis method three
-const promisesThree = new Promise(function (resolve,reject){
-    setTimeout(function(){
-        resolve({ Name:"imran", email: "imran@gmail.com"})
-    },1000)
-})
-promisesThree.then(function(userData){
-   console.log(userData);
-})
+// //promisis method three
+// const promisesThree = new Promise(function (resolve,reject){
+//     setTimeout(function(){
+//         resolve({ Name:"imran", email: "imran@gmail.com"})
+//     },1000)
+// })
+// promisesThree.then(function(userData){
+//    console.log(userData);
+// })
 
 
-//promisos four
+// //promisos four
 
-const promisesFour = new Promise( function (resolve,reject){
-    setTimeout( function (){
-        let Error = true;
-    if(!Error){
-        resolve({language: "java script", password: "kamran321" });
-    }else{
-        reject('eror: js went wrong');
-    }
-},1000)
+// const promisesFour = new Promise( function (resolve,reject){
+//     setTimeout( function (){
+//         let Error = true;
+//     if(!Error){
+//         resolve({language: "java script", password: "kamran321" });
+//     }else{
+//         reject('eror: js went wrong');
+//     }
+// },1000)
 
-})
-promisesFour.then(  (user) => {
-    console.log(user);
-    return user.language
-}).then( (language)  => {
-    console.log(language);
-}).catch( function (Error){
-    console.log(Error);
-})
+// })
+// promisesFour.then(  (user) => {
+//     console.log(user);
+//     return user.language
+// }).then( (language)  => {
+//     console.log(language);
+// }).catch( function (Error){
+//     console.log(Error);
+// })
 
 //promisis five
 
-const promisesFive = new Promise( function (resolve,reject){
-    setTimeout( function (){
-        let Error = false;
-    if(!Error){
-        resolve({userName: "java script", password: "kamran321" });
-    }else{
-        reject('eror: js went wrong');
-    }
-},1000)
+// const promisesFive = new Promise( function (resolve,reject){
+//     setTimeout( function (){
+//         let Error = false;
+//     if(!Error){
+//         resolve({userName: "java script", password: "kamran321" });
+//     }else{
+//         reject('eror: js went wrong');
+//     }
+// },1000)
 
+// })
+
+// async function consumePromiseFive(){
+//  try {
+
+//   const response = await promisesFive
+//    console.log(response);
+// } catch (Error){
+//     console.log(Error);
+// }
+// } 
+// consumePromiseFive()
+
+// fatch()
+
+
+let p = fetch("https://api.ipify.org/?format=peshawar")
+.then( (value1)=> {
+    return value1
 })
-
-async function consumePromiseFive(){
- try {
-
-  const response = await promisesFive
-   console.log(response);
-} catch (Error){
-    console.log(Error);
-}
-} 
-consumePromiseFive()
+.then( (value2) =>{
+   console.log(value2);
+})
